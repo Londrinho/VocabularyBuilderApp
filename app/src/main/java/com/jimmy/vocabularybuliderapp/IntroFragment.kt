db2b8b1c58976140ad87f59244735da0e605772c
Londrinho <jimmyapara@gmail.com>
2021-08-09 21:29:01 +0100

    This is the second version on my vocabulary builder app.

---
+++ app/src/main/java/com/jimmy/vocabularybuliderapp/IntroFragment.kt
@@ -0,0 +1,108 @@
+package com.jimmy.vocabularybuliderapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.jimmy.vocabularybuliderapp.databinding.FragmentIntroBinding
+
+
+class IntroFragment : Fragment() {
+    companion object {
+        const val TAG = "IntroFragment"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+
+    private val viewModel by viewModels<LoginViewModel>()
+    private lateinit  var binding: FragmentIntroBinding
+
+        override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View? {
+
+        binding = inflate(inflater, R.layout.fragment_intro, container, false)
+
+            return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeAuthenticationState()
+
+        binding.loginBtnFromIntro.setOnClickListener{ launchSignInFlow() }
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email
+        // If users choose to register with their email,
+        // they will need to create a password as well
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+            //
+        )
+
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_RESULT_CODE code
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), IntroFragment.SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+
+                // User successfully signed in
+                Log.i(
+                    TAG,
+                    "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun observeAuthenticationState() {
+
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    binding.loginBtnFromIntro.text = getString(R.string.logout)
+                    findNavController().navigate(R.id.action_introFragment_to_mainFragment)
+                    binding.loginBtnFromIntro.setOnClickListener {
+                        AuthUI.getInstance().signOut(requireContext())
+                    }
+                }
+
+            else -> {
+            findNavController().navigate(R.id.action_mainFragment_to_introFragment)
+                binding.loginBtnFromIntro.text = getString(R.string.login)
+                binding.loginBtnFromIntro.setOnClickListener {
+                launchSignInFlow()
+            }
+        }
+        }
+    })
+}
+}
+
---
+++ app/src/main/java/com/jimmy/vocabularybuliderapp/MainFragment.kt
@@ -0,0 +1,75 @@
+package com.jimmy.vocabularybuliderapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.jimmy.vocabularybuliderapp.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button.setOnClickListener { rollDice() }
+    }
+
+    fun rollDice(){
+        val firstDice = Dice()
+        val ranNumb = firstDice.rollTheDice()
+
+        val drawableResource =  when (ranNumb){
+            1 -> R.drawable.capricious
+            2 -> R.drawable.cumulate
+            3 -> R.drawable.egotist
+            4 -> R.drawable.exonerate
+            5 -> R.drawable.intoxicate
+            6 -> R.drawable.maculated
+            7 -> R.drawable.precocious
+            8 -> R.drawable.vindicate
+            9 -> R.drawable.perambulate
+            10 -> R.drawable.etiquette
+            11 -> R.drawable.susurrus
+            12 -> R.drawable.incipient
+            13 -> R.drawable.enormousness
+            14 -> R.drawable.filial
+            15 -> R.drawable.propitious
+            16 -> R.drawable.nascent
+            17 -> R.drawable.orgulous
+            18 -> R.drawable.febrile
+            19 -> R.drawable.risible
+            20 -> R.drawable.normenclature
+            else -> R.drawable.capricious
+        }
+
+        binding.imageView.setImageResource(drawableResource)
+
+        binding.imageView.contentDescription = ranNumb.toString()
+
+
+    }
+
+    class Dice {
+
+        fun rollTheDice(): Int {
+            val ranNumBer = (0..20).random()
+            return ranNumBer
+        }
+    }
+
+
+}
